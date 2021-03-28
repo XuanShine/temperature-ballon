cce8e13bfbc2233f21892471a6f7f2977ad97117
XuanShine <xuan.ng@hotmail.com>
2021-03-28 23:06:43 +0200

    quasi finish

--- web.py
+++ web.py
@@ -7,12 +7,18 @@ logging.basicConfig(filename=os.path.join(C, "run_server.log"), level=logging.IN
 
 from datetime import datetime
 import json
+import yaml
 
 from bottle import run, template, Bottle, route, response, Response
 from bottle import jinja2_view
 import bottle
 
 from main import main as register_temperature
+from main import extraire_temperature, lire_fichier, routes_capteurs
+import matplotlib.pyplot as plt, mpld3
+import numpy as np
+
+
 
 import socket
 def get_ip():
@@ -32,14 +38,49 @@ bottle.TEMPLATE_PATH.insert(0, os.path.join(C, 'views'))
 
 @temperature.route("/")
 def index():
-    result = ""
+    with open("capteurs.yaml", "r") as f_in:
+        capteurs_connus = yaml.safe_load(f_in.read())
+    capteurs = []
     for file in os.listdir(C):
-        if file.startswith("Capteur"):
-            with open(file, "r") as f_in:
-                result += f"{file} :\n {f_in.read()}\n{'=' * 50}\n"
-    return result.replace("\n", "<br>")
+        if "28" in file:
+            name = file[:-8]
+            capteurs.append({"name": capteurs_connus.get(name, name),
+                             "data": get_history(file)})
+    a, b = [], []
+    for data in capteurs[0]["data"]:
+        a.append(datetime.strptime(data[0], '%d/%m/%Y %H:%M:%S'))
+        b.append(data[1])
+
+
+    a = np.array(a, dtype='datetime64')
+    b = np.array(b)
+    plt.plot(a, b)
+    plt.show()
+
+
+def get_history(capteur, n_data=60*24*7):
+    with open(f"{capteur}", "r") as f_in:
+        data = f_in.readlines()
+    # data = [tuple(info.strip().split("   ")) for info in data]
+    data = list(map(lambda x: (x[0], float(x[1])),
+                map(lambda x: x.strip().split("   "),
+                data)))
+    if len(data) <= n_data:
+        return data
+    else:
+        return data[len(data) - n_data:]
 
 
 @temperature.route("/get_temperature")
 def get_temperature():
-    return "TODO"
+    """Retourne les valeurs en JSON:
+    [{nom_du_capteur: temperature}, ..]
+    """
+    with open("capteurs.yaml", "r") as f_in:
+        capteurs_connus = yaml.safe_load(f_in.read())
+    capteurs = []
+    for file in os.listdir(C):
+        if "28" in file:
+            capteurs.append({"name": capteurs_connus.get(file, "inconnu"),
+                             "id": file,
+                             "temperature": extraire_temperature(lire_fichier(os.path.join(routes_capteurs, file)))})
